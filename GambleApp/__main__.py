283fccc3e7b65ca22f17e3130644dd4c86f024a8
codelao <codelao.py@gmail.com>
2026-01-15 00:40:33 -0600

    shebang fix

--- GambleApp/__main__.py
+++ GambleApp/__main__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 #	 .=============-\  .=============-\ 
 #	 |             ||  |             || 
 #	 |  ●          ||  |   ●     ●   || 
@@ -9,7 +11,6 @@
 #	       CLI              GAMBLE      
 #	~[$$$__________by Lao__________$$$]~
 
-#!/usr/bin/env python3
 
 import sys
 from .gamble import main
